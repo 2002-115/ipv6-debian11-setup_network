1b64eaa69960896baf95a7d71ac32d948aa70ea1
2002-115 <85139299+2002-115@users.noreply.github.com>
2024-07-15 20:28:31 +0700

    Update setup_network.sh

--- setup_network.sh
+++ setup_network.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# cap nhat
+
+sudo apt update && sudo apt upgrade -y && sudo apt install wget -y
+
+
+sudo apt-get install zip curl openssl
+
 # Mở tệp /etc/network/interfaces và thêm cấu hình IPv6
 echo -e "\n# IPv6 configuration for the primary network interface\niface enp1s0 inet6 auto" | sudo tee -a /etc/network/interfaces
 
